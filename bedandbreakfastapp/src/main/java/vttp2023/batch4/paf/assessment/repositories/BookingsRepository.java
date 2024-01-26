c652546120e295f78e584eaeb1e60a6d63ae2975
Jaspereap <jaspereap@gmail.com>
2024-01-26 15:31:50 +0800

    Final

--- bedandbreakfastapp/src/main/java/vttp2023/batch4/paf/assessment/repositories/BookingsRepository.java
+++ bedandbreakfastapp/src/main/java/vttp2023/batch4/paf/assessment/repositories/BookingsRepository.java
@@ -1,8 +1,10 @@
 package vttp2023.batch4.paf.assessment.repositories;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
@@ -34,26 +36,32 @@ public class BookingsRepository {
 	// TODO: Task 6
 	// IMPORTANT: DO NOT MODIFY THE SIGNATURE OF THIS METHOD.
 	// You may only add throw exceptions to this method
-	public void newUser(User user) {
+	public void newUser(User user) throws SQLException {
 		System.out.println("Adding user to SQL 'user'");
 		String query = """
 				INSERT INTO users VALUES (?, ?)
 				""";
-		template.update(query, user.email(), user.name());
+		int updated = template.update(query, user.email(), user.name());
+		if (!(updated > 0)) {
+			throw new SQLException("Failed to add user");
+		}
 	}
 
 	// TODO: Task 6
 	// IMPORTANT: DO NOT MODIFY THE SIGNATURE OF THIS METHOD.
 	// You may only add throw exceptions to this method
-	public void newBookings(Bookings bookings) {
+	public void newBookings(Bookings bookings) throws SQLException {
 		System.out.println("Adding booking to SQL 'bookings'");
 		String query = """
 				INSERT INTO bookings VALUES (?, ?, ?, ?)
 				""";
-		template.update(query,
-			bookings.getBookingId(),
-			bookings.getListingId(),
-			bookings.getDuration(),
-			bookings.getEmail());
+		int updated = template.update(query,
+						bookings.getBookingId(),
+						bookings.getListingId(),
+						bookings.getDuration(),
+						bookings.getEmail());
+		if (!(updated > 0)) {
+			throw new SQLException("Failed to add user");
+		}
 	}
 }
